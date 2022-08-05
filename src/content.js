6c4dada7f6102dce4ebcf7fb60914a29213a1128
CharlieS1103 <someonelikeswindows@gmail.com>
2022-08-04 21:43:56 -0400

    Change from beta to new

--- src/content.js
+++ src/content.js
@@ -1,7 +1,7 @@
 
         const projects_tab = document.querySelector("#projects-tab")
-        projects_tab.href = projects_tab.href.replace("?type=beta","");
-         projects_tab.id = projects_tab.id.replace("?type=beta","");
+        projects_tab.href = projects_tab.href.replace("?type=new","");
+         projects_tab.id = projects_tab.id.replace("?type=new","");
     	function countString(str, letter) {
     let count = 0;
 
