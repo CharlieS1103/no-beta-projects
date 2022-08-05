df6b69a2d7f7c279e6dbf7bcdbd2c5f3da23d539
CharlieS1103 <someonelikeswindows@gmail.com>
2022-08-04 21:46:14 -0400

    remove unnecessary function

--- src/content.js
+++ src/content.js
@@ -2,16 +2,3 @@
         const projects_tab = document.querySelector("#projects-tab")
         projects_tab.href = projects_tab.href.replace("?type=new","");
          projects_tab.id = projects_tab.id.replace("?type=new","");
-    	function countString(str, letter) {
-    let count = 0;
-
-    // looping through the items
-    for (let i = 0; i < str.length; i++) {
-
-        // check if the character is at that position
-        if (str.charAt(i) == letter) {
-            count += 1;
-        }
-    }
-    return count;
-}
